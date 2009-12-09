14185a5f4537c3cf9039c86ee76959cb2b43e487
Ben Cochran <bcochran@gmail.com>
2009-12-09 13:56:13 -0600

    Removed GNDistAndLandmark references

--- Classes/LiveViewController.m
+++ Classes/LiveViewController.m
@@ -225,9 +225,9 @@
 	
 	NSArray *landmarks = [[GNLayerManager sharedManager] getNClosestLandmarks:10 toLocation:newLocation maxDistance:10];
 	ARGeoCoordinate *coordinate;
-	for (GNDistAndLandmark *gndl in landmarks) {
-		coordinate = [ARGeoCoordinate coordinateWithLocation:gndl.landmark];
-		coordinate.title = gndl.landmark.name;
+	for (GNLandmark *landmark in landmarks) {
+		coordinate = [ARGeoCoordinate coordinateWithLocation:landmark];
+		coordinate.title = landmark.name;
 		
 		[self.arViewController addCoordinate:coordinate];
 	}	
